0c5657e9ba3c3dcc7c43c87c0b1dac5d6d051a6a
Didier Contreras <didier9133@gmail.com>
2024-07-08 12:07:11 -0400

    fix msg welcome page

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,12 @@
-import { App, GitHubSourceCodeProvider } from "@aws-cdk/aws-amplify-alpha";
+import {
+  App,
+  GitHubSourceCodeProvider,
+  Platform,
+  RedirectStatus,
+} from "@aws-cdk/aws-amplify-alpha";
 import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,10 +15,43 @@ export class InfraStack extends Stack {
     const amplifyApp = new App(this, "amplify-app", {
       appName: "Nextjs hosting example",
       sourceCodeProvider: new GitHubSourceCodeProvider({
-        owner: "aws-samples",
-        repository: "amplify-nextjs",
-        oauthToken: SecretValue.secretsManager(""),
+        owner: "didier9133",
+        repository: "organic-shop",
+        oauthToken: SecretValue.secretsManager("github-token-cdk"),
       }),
+      autoBranchDeletion: true,
+      platform: Platform.WEB_COMPUTE,
+      customRules: [
+        {
+          source: "/<*>",
+          target: "/index.html",
+          status: RedirectStatus.NOT_FOUND_REWRITE,
+        },
+      ],
+      buildSpec: BuildSpec.fromObject({
+        version: "1.0",
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: ["npm ci"],
+            },
+            build: {
+              commands: ["npm run build"],
+            },
+          },
+          artifacts: {
+            baseDirectory: ".next",
+            files: ["**/*"],
+          },
+          cache: {
+            paths: ["node_modules/**/*"],
+          },
+        },
+      }),
+    });
+
+    amplifyApp.addBranch("main", {
+      stage: "PRODUCTION",
     });
   }
 }
